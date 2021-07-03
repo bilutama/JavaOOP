2c575e9ff151dc0e6f7127bd02620a97af6c1de6
Maksim Biluta <biluta.m.a@gmail.com>
2021-07-03 15:25:25 +0200

    CSV amendments, 3 July

--- Csv/src/ru/academits/biluta/csv_converter/CsvConverter.java
+++ Csv/src/ru/academits/biluta/csv_converter/CsvConverter.java
@@ -6,196 +6,191 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class CsvConverter {
-    public static void convertCsvToHtmlTable(String inputFileName, String outputFileName) {
-        try (Scanner scanner = new Scanner(new FileInputStream(inputFileName));
-             PrintWriter writer = new PrintWriter(outputFileName)) {
+    public static void convertCsvToHtmlTable(String inputFileName, String outputFileName) throws IOException {
+        Scanner scanner = new Scanner(new FileInputStream(inputFileName));
+        PrintWriter writer = new PrintWriter(outputFileName);
 
-            final char SEPARATOR = ',';
-            final char QUOTES = '\"';
-            final char END_OF_STRING = '\n';
+        final char SEPARATOR = ',';
+        final char QUOTES = '\"';
+        final char END_OF_STRING = '\n';
 
-            final String TABLE_OPEN_TAG = "<table>";
-            final String TABLE_CLOSE_TAG = "</table>";
-            final String ROW_OPEN_TAG = "<tr>";
-            final String ROW_CLOSE_TAG = "</tr>";
-            final String CELL_OPEN_TAG = "<td>";
-            final String CELL_CLOSE_TAG = "</td>";
-            final String BREAK_LINE_TAG = "<br/>";
+        final String TABLE_OPEN_TAG = "<table>";
+        final String TABLE_CLOSE_TAG = "</table>";
+        final String ROW_OPEN_TAG = "<tr>";
+        final String ROW_CLOSE_TAG = "</tr>";
+        final String CELL_OPEN_TAG = "<td>";
+        final String CELL_CLOSE_TAG = "</td>";
+        final String BREAK_LINE_TAG = "<br/>";
 
-            boolean separatorMode = true;
-            boolean isNewCell = true;
-            boolean isEscapeQuotes = false;
+        boolean separatorMode = true;
+        boolean isNewCell = true;
+        boolean isEscapeQuotes = false;
 
-            writer.println("<!DOCTYPE html>" + System.lineSeparator() +
-                    "<html>" + System.lineSeparator() +
-                    "<head>" + System.lineSeparator() +
-                    "<meta charset=\"utf-8\">");
-            writer.println("<title>Generated from " + inputFileName + "</title>");
-            writer.println("</head>" + System.lineSeparator() + "<body>");
+        writer.println("<!DOCTYPE html>" + System.lineSeparator() +
+                "<html>" + System.lineSeparator() +
+                "<head>" + System.lineSeparator() +
+                "<meta charset=\"utf-8\">");
+        writer.println("<title>Generated from " + inputFileName + "</title>");
+        writer.println("</head>" + System.lineSeparator() + "<body>");
 
-            writer.println(TABLE_OPEN_TAG);
-            String processedString;
+        writer.println(TABLE_OPEN_TAG);
+        String processedString;
 
-            while (scanner.hasNextLine()) {
-                processedString = scanner.nextLine();
-                int currentCharIndex = 0;
+        while (scanner.hasNextLine()) {
+            processedString = scanner.nextLine();
+            int currentCharIndex = 0;
 
-                while (currentCharIndex < processedString.length()) {
-                    char currentChar = processedString.charAt(currentCharIndex);
-                    char nextChar = (currentCharIndex < processedString.length() - 1) ? processedString.charAt(currentCharIndex + 1) : END_OF_STRING;
+            while (currentCharIndex < processedString.length()) {
+                char currentChar = processedString.charAt(currentCharIndex);
+                char nextChar = (currentCharIndex < processedString.length() - 1) ? processedString.charAt(currentCharIndex + 1) : END_OF_STRING;
 
-                    if (currentCharIndex == 0 && isNewCell) {
-                        writer.println(ROW_OPEN_TAG);
+                if (currentCharIndex == 0 && isNewCell) {
+                    writer.println(ROW_OPEN_TAG);
 
-                        // Обработка если первая ячейка в строке пустая
-                        if (currentChar == SEPARATOR) {
-                            writer.print(CELL_OPEN_TAG);
-                            writer.println(CELL_CLOSE_TAG);
-                        }
-                    }
-
-                    // Обработка начала новой ячейки по флагу
-                    if (isNewCell) {
+                    // Обработка если первая ячейка в строке пустая
+                    if (currentChar == SEPARATOR) {
                         writer.print(CELL_OPEN_TAG);
+                        writer.println(CELL_CLOSE_TAG);
+                    }
+                }
 
-                        // Проверка и финализация ячейки, если она пустая
-                        if (currentChar == SEPARATOR) {
-                            if (nextChar == SEPARATOR) {
-                                writer.println(CELL_CLOSE_TAG);
-                                ++currentCharIndex;
-                                continue;
-                            }
+                // Обработка начала новой ячейки по флагу
+                if (isNewCell) {
+                    writer.print(CELL_OPEN_TAG);
 
-                            if (nextChar == END_OF_STRING) {
-                                writer.println(CELL_CLOSE_TAG);
-                                writer.println(ROW_CLOSE_TAG);
-                                ++currentCharIndex;
-                                continue;
-                            }
-
-                            if (nextChar == QUOTES) {
-                                isNewCell = false;
-                                separatorMode = false;
-                                currentCharIndex += 2;
-                                continue;
-                            }
-
-                            isNewCell = false;
+                    // Проверка и финализация ячейки, если она пустая
+                    if (currentChar == SEPARATOR) {
+                        if (nextChar == SEPARATOR) {
+                            writer.println(CELL_CLOSE_TAG);
                             ++currentCharIndex;
                             continue;
                         }
 
-                        if (currentChar == QUOTES) {
+                        if (nextChar == END_OF_STRING) {
+                            writer.println(CELL_CLOSE_TAG);
+                            writer.println(ROW_CLOSE_TAG);
+                            ++currentCharIndex;
+                            continue;
+                        }
+
+                        if (nextChar == QUOTES) {
                             isNewCell = false;
                             separatorMode = false;
-                            ++currentCharIndex;
+                            currentCharIndex += 2;
                             continue;
                         }
 
                         isNewCell = false;
-                        continue;
-                    }
-
-                    // Блок кода для режима чтения separatorMode, т.е. не в кавычках
-                    if (separatorMode) {
-                        // Если nextChar - разделитель, то
-                        // записываем символ и финализируем ячейку
-                        if (nextChar == SEPARATOR) {
-                            writer.print(getStringWithReplacements(currentChar));
-                            writer.println(CELL_CLOSE_TAG);
-
-                            isNewCell = true;
-                            ++currentCharIndex;
-                            continue;
-                        }
-
-                        // Если nextChar - конец строки, финализируем ячейку и строку
-                        if (nextChar == END_OF_STRING) {
-                            writer.print(getStringWithReplacements(currentChar));
-                            writer.println(CELL_CLOSE_TAG);
-                            writer.println(ROW_CLOSE_TAG);
-
-                            isNewCell = true;
-                            ++currentCharIndex;
-                            continue;
-                        }
-
-                        writer.print(getStringWithReplacements(currentChar));
                         ++currentCharIndex;
                         continue;
                     }
 
-                    // Блок кода для режима чтения !separatorMode, т.е. когда содержимое ячейки в кавычках
                     if (currentChar == QUOTES) {
-                        // Если предыдущий символ были экранирующие кавычки,
-                        // то эти кавычки нужно записать и выключить флаг,
-                        if (isEscapeQuotes) {
-                            writer.print(currentChar);
-                            isEscapeQuotes = false;
-
-                            // Если сразу за ними конец строки, записываем тэг разрыва строки.
-                            if (nextChar == END_OF_STRING) {
-                                writer.print(BREAK_LINE_TAG);
-                            }
-
-                            ++currentCharIndex;
-                            continue;
-                        }
-
-                        // Если nextChar тоже кавычки, ставим флаг isEscapeQuotes и переходим на следующий символ
-                        if (nextChar == QUOTES) {
-                            isEscapeQuotes = true;
-                            ++currentCharIndex;
-                            continue;
-                        }
-
-                        // Если nextChar - разделитель, то финализируем ячейку
-                        if (nextChar == SEPARATOR) {
-                            writer.println(CELL_CLOSE_TAG);
-
-                            separatorMode = true;
-                            isNewCell = true;
-                            ++currentCharIndex;
-                            continue;
-                        }
-
-                        // Если nextChar - конец строки, то финализируем ячейку и строку
-                        if (nextChar == END_OF_STRING) {
-                            writer.println(CELL_CLOSE_TAG);
-                            writer.println(ROW_CLOSE_TAG);
-
-                            separatorMode = true;
-                            isNewCell = true;
-                            ++currentCharIndex;
-                            continue;
-                        }
+                        isNewCell = false;
+                        separatorMode = false;
+                        ++currentCharIndex;
+                        continue;
                     }
 
-                    // Если nextChar - конец строки, то вставляем символ и
-                    // добавляем тэг переноса строки
-                    // Далее строка заканчивается и читается следующая из файла.
+                    isNewCell = false;
+                    continue;
+                }
+
+                // Блок кода для режима чтения separatorMode, т.е. не в кавычках
+                if (separatorMode) {
+                    // Если nextChar - разделитель, то
+                    // записываем символ и финализируем ячейку
+                    if (nextChar == SEPARATOR) {
+                        writer.print(getStringWithReplacements(currentChar));
+                        writer.println(CELL_CLOSE_TAG);
+
+                        isNewCell = true;
+                        ++currentCharIndex;
+                        continue;
+                    }
+
+                    // Если nextChar - конец строки, финализируем ячейку и строку
                     if (nextChar == END_OF_STRING) {
                         writer.print(getStringWithReplacements(currentChar));
-                        writer.print(BREAK_LINE_TAG);
+                        writer.println(CELL_CLOSE_TAG);
+                        writer.println(ROW_CLOSE_TAG);
+
+                        isNewCell = true;
+                        ++currentCharIndex;
+                        continue;
+                    }
+
+                    writer.print(getStringWithReplacements(currentChar));
+                    ++currentCharIndex;
+                    continue;
+                }
+
+                // Блок кода для режима чтения !separatorMode, т.е. когда содержимое ячейки в кавычках
+                if (currentChar == QUOTES) {
+                    // Если предыдущий символ были экранирующие кавычки,
+                    // то эти кавычки нужно записать и выключить флаг,
+                    if (isEscapeQuotes) {
+                        writer.print(currentChar);
+                        isEscapeQuotes = false;
+
+                        // Если сразу за ними конец строки, записываем тэг разрыва строки.
+                        if (nextChar == END_OF_STRING) {
+                            writer.print(BREAK_LINE_TAG);
+                        }
 
                         ++currentCharIndex;
                         continue;
                     }
 
-                    // Если никакое условие не сработало,
-                    // то записываем в файл текущий символ
-                    writer.print(getStringWithReplacements(currentChar));
-                    ++currentCharIndex;
+                    // Если nextChar тоже кавычки, ставим флаг isEscapeQuotes и переходим на следующий символ
+                    if (nextChar == QUOTES) {
+                        isEscapeQuotes = true;
+                        ++currentCharIndex;
+                        continue;
+                    }
+
+                    // Если nextChar - разделитель, то финализируем ячейку
+                    if (nextChar == SEPARATOR) {
+                        writer.println(CELL_CLOSE_TAG);
+
+                        separatorMode = true;
+                        isNewCell = true;
+                        ++currentCharIndex;
+                        continue;
+                    }
+
+                    // Если nextChar - конец строки, то финализируем ячейку и строку
+                    if (nextChar == END_OF_STRING) {
+                        writer.println(CELL_CLOSE_TAG);
+                        writer.println(ROW_CLOSE_TAG);
+
+                        separatorMode = true;
+                        isNewCell = true;
+                        ++currentCharIndex;
+                        continue;
+                    }
                 }
+
+                // Если nextChar - конец строки, то вставляем символ и
+                // добавляем тэг переноса строки
+                // Далее строка заканчивается и читается следующая из файла.
+                if (nextChar == END_OF_STRING) {
+                    writer.print(getStringWithReplacements(currentChar));
+                    writer.print(BREAK_LINE_TAG);
+
+                    ++currentCharIndex;
+                    continue;
+                }
+
+                // Если никакое условие не сработало,
+                // то записываем в файл текущий символ
+                writer.print(getStringWithReplacements(currentChar));
+                ++currentCharIndex;
             }
-
-            writer.println(TABLE_CLOSE_TAG);
-            writer.print("</body>" + System.lineSeparator() + "</html>");
-
-            System.out.printf("Success! See \"%s\"%n", outputFileName);
-        } catch (IOException exception) {
-            System.out.printf("File %s doesn't exist.", inputFileName);
         }
+
+        writer.println(TABLE_CLOSE_TAG);
+        writer.print("</body>" + System.lineSeparator() + "</html>");
     }
 
     private static String getStringWithReplacements(char charToReplace) {
@@ -208,22 +203,5 @@ public class CsvConverter {
         } else {
             return Character.toString(charToReplace);
         }
-    }
-
-    public static void printHelpMessage() {
-        System.out.printf("Wrong arguments.%n%n");
-
-        System.out.printf("NAME%n");
-        System.out.printf("\tru.academits.biluta.CsvConverter.CsvConverter - converting csv into html-table.%n%n");
-
-        System.out.printf("SYNOPSIS%n");
-        System.out.printf("\tru.academits.biluta.CsvConverter.CsvConverter [input_filename] [output_filename]%n%n");
-
-        System.out.printf("DESCRIPTION%n");
-        System.out.printf("\tTwo arguments must be provided:%n%n");
-        System.out.printf("\tinput_filename - input csv-file name (String) with delimiters%n%n");
-        System.out.printf("\toutput_filename - output html-file name (String) for generated table,%n" +
-                "\t\tpreferably to have .html or .htm extension.%n" +
-                "\t\tIf file exists it will be overwritten, otherwise created.%n");
     }
 }
--- Csv/src/ru/academits/biluta/csv_main/Main.java
+++ Csv/src/ru/academits/biluta/csv_main/Main.java
@@ -2,21 +2,53 @@ package ru.academits.biluta.csv_main;
 
 import ru.academits.biluta.csv_converter.CsvConverter;
 
-public class Main {
-    public static void main(String[] args) {
-        if (args.length != 2) {
-            CsvConverter.printHelpMessage();
-            return;
-        }
+import java.io.IOException;
 
-        if (args[0].equals(args[1])) {
-            System.out.printf("The output file must be different from the input file.%n");
+public class Main {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            printHelpMessage();
             return;
         }
 
         String inputFileName = args[0];
         String outputFileName = args[1];
 
+        if (inputFileName.equals(outputFileName)) {
+            System.out.println("The output file must be different from the input file.");
+            return;
+        }
+
+        System.out.println(inputFileName);
+        System.out.println(outputFileName);
+
         CsvConverter.convertCsvToHtmlTable(inputFileName, outputFileName);
+//        try {
+//            System.out.printf("Success! See \"%s\"%n", outputFileName);
+//        } catch (IOException exception) {
+//            System.out.printf("File %s doesn't exist.", inputFileName);
+//        }
+    }
+
+    public static void printHelpMessage() {
+        System.out.println("Wrong arguments.");
+        System.out.println();
+
+        System.out.println("NAME");
+        System.out.println("\tru.academits.biluta.CsvConverter.CsvConverter - converting csv into html-table.");
+        System.out.println();
+
+        System.out.println("SYNOPSIS");
+        System.out.println("\tru.academits.biluta.CsvConverter.CsvConverter [input_filename] [output_filename]");
+        System.out.println();
+
+        System.out.println("DESCRIPTION");
+        System.out.println("\tTwo arguments must be provided:");
+        System.out.println();
+        System.out.println("\tinput_filename - input csv-file name (String) with delimiters");
+        System.out.println();
+        System.out.println("\toutput_filename - output html-file name (String) for generated table,");
+        System.out.println("\t\tpreferably to have .html or .htm extension.");
+        System.out.println("\t\tIf file exists it will be overwritten, otherwise created.");
     }
 }
