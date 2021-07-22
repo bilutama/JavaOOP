8eb8c1c027a163cf88e1b5ee3a4b380754d7a79b
Maksim Biluta <biluta.m.a@gmail.com>
2021-07-23 00:14:07 +0200

    SinglyLinkedList

--- SinglyLinkedList/src/ru/academits/biluta/list_main/Main.java
+++ SinglyLinkedList/src/ru/academits/biluta/list_main/Main.java
@@ -7,6 +7,7 @@ import java.util.NoSuchElementException;
 public class Main {
     public static void main(String[] args) {
         try {
+            //123
             SinglyLinkedList<String> list0 = new SinglyLinkedList<>(null);
             System.out.println(list0);
 
