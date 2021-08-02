27fb0ecb51c2e6efa2503440b7780649f576e3b0
Maksim Biluta <biluta.m.a@gmail.com>
2021-08-02 02:01:26 +0200

    HashTable

--- HashTable/src/ru/academits/biluta/hash_table/HashTable.java
+++ HashTable/src/ru/academits/biluta/hash_table/HashTable.java
@@ -1,21 +1,55 @@
 package ru.academits.biluta.hash_table;
 
+import ru.academits.biluta.list.SinglyLinkedList;
+
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class HashTable<T> implements Collection<T> {
+    final int DEFAULT_CAPACITY = 30;
+
+    private LinkedList<T>[] hashTable;
+    private int size;
+    private int capacity;
+    private double loadRatio;
+
+    public HashTable() {
+        hashTable = (LinkedList<T>[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public HashTable(int capacity) {
+        if (capacity < DEFAULT_CAPACITY) {
+            throw new IllegalArgumentException(String.format("Capacity %d has to be not less than %d", capacity, DEFAULT_CAPACITY));
+        }
+
+        hashTable = (LinkedList<T>[]) new Object[capacity];
+    }
+
+    private void updateLoadRatio() {
+        loadRatio = (double) size / capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    private int getItemHashCode(T item) {
+        return Math.abs(item.hashCode() % capacity);
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
-    public boolean contains(Object o) {
+    public boolean contains(Object object) {
         return false;
     }
 
