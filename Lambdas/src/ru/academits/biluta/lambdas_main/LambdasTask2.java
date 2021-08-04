91d492ad7627d949613e92dbcd661d4c8d119d48
bilutama <biluta.m.a@gmail.com>
2021-08-04 13:07:58 +0200

    Lambdas

--- Lambdas/src/ru/academits/biluta/lambdas_main/LambdasTask2.java
+++ Lambdas/src/ru/academits/biluta/lambdas_main/LambdasTask2.java
@@ -1,16 +1,21 @@
 package ru.academits.biluta.lambdas_main;
 
+import ru.academits.biluta.lambdas.FibonacciNumbers;
+
 import java.util.Scanner;
 import java.util.stream.DoubleStream;
+import java.util.stream.LongStream;
 
 public class LambdasTask2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter numbers count:");
-        int numberCount = scanner.nextInt();
+        System.out.println("Enter square roots count:");
+        int numbersCount = scanner.nextInt();
+        DoubleStream.iterate(1, x -> x + 1).limit(numbersCount).map(Math::sqrt).forEach(x -> System.out.printf("%.3f%n", x));
 
-        System.out.println("Square roots from a stream:");
-        DoubleStream.iterate(1, x -> x + 1).limit(numberCount).map(Math::sqrt).forEach(x -> System.out.printf("%.3f%n", x));
+        System.out.println("Enter Fibonacci numbers count (less or equal to 92):");
+        numbersCount = scanner.nextInt();
+        LongStream.iterate(1, x -> x + 1).limit(numbersCount).map(FibonacciNumbers::getFibonacciNumber).forEach(System.out::println);
     }
 }
