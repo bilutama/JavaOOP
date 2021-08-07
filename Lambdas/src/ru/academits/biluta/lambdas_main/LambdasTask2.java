d39d73804cbc7c8daf54a8cbb765815b0860905e
Maksim Biluta <biluta.m.a@gmail.com>
2021-08-07 16:38:05 +0200

    Lambdas

--- Lambdas/src/ru/academits/biluta/lambdas_main/LambdasTask2.java
+++ Lambdas/src/ru/academits/biluta/lambdas_main/LambdasTask2.java
@@ -8,24 +8,33 @@ import java.util.stream.Stream;
 
 public class LambdasTask2 {
     public static void main(String[] args) {
-        int FIBONACCI_NUMBER_MAXIMUM_INDEX = 92;
+        int LONG_FIBONACCI_NUMBER_MAXIMUM_INDEX = 92;
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter square roots count:");
         int numbersCount = scanner.nextInt();
-        DoubleStream.iterate(1, x -> x + 1).limit(numbersCount).map(Math::sqrt).forEach(x -> System.out.printf("%.3f%n", x));
+        DoubleStream.iterate(1, x -> x + 1)
+                .limit(numbersCount)
+                .map(Math::sqrt)
+                .forEach(x -> System.out.printf("%.3f%n", x));
 
         System.out.println("Enter Fibonacci numbers count (less or equal to 92):");
         numbersCount = scanner.nextInt();
 
         // Fibonacci numbers stream for specific indices, i.e. for any index iterator (for example, x -> x + 1, x -> 2 * x or x -> x + 3)
-        Stream.iterate(1, x -> x + 1).limit(Math.min(numbersCount, FIBONACCI_NUMBER_MAXIMUM_INDEX)).map(FibonacciNumbers::getFibonacciNumber).forEach(System.out::println);
+        Stream.iterate(1, x -> x + 1)
+                .limit(Math.min(numbersCount, LONG_FIBONACCI_NUMBER_MAXIMUM_INDEX))
+                .map(FibonacciNumbers::getFibonacciNumber)
+                .forEach(System.out::println);
 
         System.out.println("Enter Fibonacci numbers count (less or equal to 92) - simplified stream:");
         numbersCount = scanner.nextInt();
 
         // Simplified Fibonacci numbers stream for standard indices sequence 1, 2, 3, ...
-        Stream.iterate(new long[]{0, 1}, x -> new long[]{x[1], x[0] + x[1]}).limit(Math.min(numbersCount, FIBONACCI_NUMBER_MAXIMUM_INDEX)).map(x -> x[1]).forEach(System.out::println);
+        Stream.iterate(new long[]{0, 1}, x -> new long[]{x[1], x[0] + x[1]})
+                .limit(Math.min(numbersCount, LONG_FIBONACCI_NUMBER_MAXIMUM_INDEX))
+                .map(x -> x[1])
+                .forEach(System.out::println);
     }
 }
