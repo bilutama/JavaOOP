a30ea190ece6fdc974535adb1ad4ed13e86c767b
Maksim Biluta <biluta.m.a@gmail.com>
2021-08-07 16:34:55 +0200

    Lambdas

--- Lambdas/src/ru/academits/biluta/lambdas_main/LambdasTask2.java
+++ Lambdas/src/ru/academits/biluta/lambdas_main/LambdasTask2.java
@@ -8,6 +8,8 @@ import java.util.stream.Stream;
 
 public class LambdasTask2 {
     public static void main(String[] args) {
+        int FIBONACCI_NUMBER_MAXIMUM_INDEX = 92;
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter square roots count:");
@@ -18,12 +20,12 @@ public class LambdasTask2 {
         numbersCount = scanner.nextInt();
 
         // Fibonacci numbers stream for specific indices, i.e. for any index iterator (for example, x -> x + 1, x -> 2 * x or x -> x + 3)
-        Stream.iterate(1, x -> x + 1).limit(Math.min(numbersCount, 92)).map(FibonacciNumbers::getFibonacciNumber).forEach(System.out::println);
+        Stream.iterate(1, x -> x + 1).limit(Math.min(numbersCount, FIBONACCI_NUMBER_MAXIMUM_INDEX)).map(FibonacciNumbers::getFibonacciNumber).forEach(System.out::println);
 
         System.out.println("Enter Fibonacci numbers count (less or equal to 92) - simplified stream:");
         numbersCount = scanner.nextInt();
 
         // Simplified Fibonacci numbers stream for standard indices sequence 1, 2, 3, ...
-        Stream.iterate(new long[]{0, 1}, x -> new long[]{x[1], x[0] + x[1]}).limit(Math.min(numbersCount, 92)).map(x -> x[1]).forEach(System.out::println);
+        Stream.iterate(new long[]{0, 1}, x -> new long[]{x[1], x[0] + x[1]}).limit(Math.min(numbersCount, FIBONACCI_NUMBER_MAXIMUM_INDEX)).map(x -> x[1]).forEach(System.out::println);
     }
 }
