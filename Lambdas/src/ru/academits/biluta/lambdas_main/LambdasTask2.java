a70fcca352bd8e75530bc5b745741486ec261b70
Maksim Biluta <biluta.m.a@gmail.com>
2021-08-07 15:34:19 +0200

    Lambdas

--- Lambdas/src/ru/academits/biluta/lambdas_main/LambdasTask2.java
+++ Lambdas/src/ru/academits/biluta/lambdas_main/LambdasTask2.java
@@ -4,7 +4,7 @@ import ru.academits.biluta.lambdas.FibonacciNumbers;
 
 import java.util.Scanner;
 import java.util.stream.DoubleStream;
-import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class LambdasTask2 {
     public static void main(String[] args) {
@@ -16,6 +16,14 @@ public class LambdasTask2 {
 
         System.out.println("Enter Fibonacci numbers count (less or equal to 92):");
         numbersCount = scanner.nextInt();
-        LongStream.iterate(1, x -> x + 1).limit(numbersCount).map(FibonacciNumbers::getFibonacciNumber).forEach(System.out::println);
+
+        // Fibonacci numbers stream for specific indices, i.e. for any index iterator (x -> 2*x or x -> x + 3)
+        Stream.iterate(1, x -> x + 1).limit(Math.min(numbersCount, 92)).map(FibonacciNumbers::getFibonacciNumber).forEach(System.out::println);
+
+        System.out.println("Enter Fibonacci numbers count (less or equal to 92) - simplified stream:");
+        numbersCount = scanner.nextInt();
+
+        // Simplified Fibonacci numbers stream for standard indices sequence 1, 2, 3, ...
+        Stream.iterate(new long[]{0, 1}, x -> new long[]{x[1], x[0] + x[1]}).limit(Math.min(numbersCount, 92)).map(x -> x[1]).forEach(System.out::println);
     }
 }
