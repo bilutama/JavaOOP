fb7a0a6c17882c5186d068d035e5f1dd582a6f50
bilutama <biluta.m.a@gmail.com>
2021-08-11 18:03:24 +0200

    javax

--- Tree/src/ru/academits/biluta/tree_main/Main.java
+++ Tree/src/ru/academits/biluta/tree_main/Main.java
@@ -1,4 +1,14 @@
 package ru.academits.biluta.tree_main;
 
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("My first GUI");
+            frame.setSize(300, 200);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true); // показать фрейм
+        });
+    }
 }
