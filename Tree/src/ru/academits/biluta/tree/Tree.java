a5825c2676e2eb17da59ce7938c7327a199f2d0d
Maksim Biluta <biluta.m.a@gmail.com>
2021-08-20 17:48:56 +0300

    Tree

--- Tree/src/ru/academits/biluta/tree/Tree.java
+++ Tree/src/ru/academits/biluta/tree/Tree.java
@@ -1,4 +1,25 @@
 package ru.academits.biluta.tree;
 
-public class Tree {
+public class Tree<T> {
+    private int size;
+
+    public Tree() {
+    }
+
+    public boolean add(T data) {
+
+        return true;
+    }
+
+    public boolean find(T data){
+        return false;
+    }
+
+    public boolean removeFirst(T data) {
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
 }
---
+++ Tree/src/ru/academits/biluta/tree/TreeNode.java
@@ -0,0 +1,7 @@
+package ru.academits.biluta.tree;
+
+public class TreeNode<T> {
+    private TreeNode<T> left;
+    private TreeNode<T> right;
+    private T data;
+}
