86c7a031cd32b73063e1e59639c52ae593dc611c
Maksim Biluta <biluta.m.a@gmail.com>
2021-08-20 16:31:38 +0300

    new modules

--- Tree/src/ru/academits/biluta/tree_main/Main.java
+++ Tree/src/ru/academits/biluta/tree_main/Main.java
@@ -1,14 +1,6 @@
 package ru.academits.biluta.tree_main;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("My first GUI");
-            frame.setSize(300, 200);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setVisible(true); // показать фрейм
-        });
     }
 }
