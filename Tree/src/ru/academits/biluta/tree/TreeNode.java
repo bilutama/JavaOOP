c7a208d40469bcfcf6f944bf7db5a111b23f3ad4
Maksim Biluta <biluta.m.a@gmail.com>
2021-08-25 01:32:51 +0200

    Tree

--- Tree/src/ru/academits/biluta/tree/TreeNode.java
+++ Tree/src/ru/academits/biluta/tree/TreeNode.java
@@ -8,4 +8,28 @@ public class TreeNode<T> {
     public TreeNode(T data) {
         this.data = data;
     }
+
+    public T getData () {
+        return data;
+    }
+
+    public void setData (T data) {
+        this.data = data;
+    }
+
+    public TreeNode<T> getLeft () {
+        return left;
+    }
+
+    public void setLeft (TreeNode<T> left) {
+        this.left = left;
+    }
+
+    public TreeNode<T> getRight () {
+        return right;
+    }
+
+    public void setRight (TreeNode<T> right) {
+        this.right = right;
+    }
 }
