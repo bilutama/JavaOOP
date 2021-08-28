c5255a5d6c0718ef3a347df78c8b62cec310efc5
bilutama <Borntobewild282>
2021-08-28 16:35:11 +0200

    Graph

--- Graph/src/ru/academits/biluta/graph_main/Main.java
+++ Graph/src/ru/academits/biluta/graph_main/Main.java
@@ -4,6 +4,7 @@ import ru.academits.biluta.graph.Graph;
 
 public class Main {
     public static void main(String[] args) {
+        Integer[] graphElements = {1, 2, 3, 4, 5, 6, 7};
         int[][] connections = {
                 {0, 1, 0, 0, 0, 0, 0},
                 {1, 0, 1, 1, 1, 1, 0},
@@ -14,9 +15,7 @@ public class Main {
                 {0, 0, 1, 0, 0, 1, 0}
         };
 
-        Integer[] graphElements = {1, 2, 3, 4, 5, 6, 7};
         Graph<Integer> graph = new Graph<>(graphElements, connections);
-
         graph.breadthTraversal();
     }
 }
