f89a630a35965b702b008473b7a65a0fcef5df5a
Maksim Biluta <biluta.m.a@gmail.com>
2021-08-28 16:32:46 +0200

    Graph

--- Graph/src/ru/academits/biluta/graph/Graph.java
+++ Graph/src/ru/academits/biluta/graph/Graph.java
@@ -1,4 +1,51 @@
 package ru.academits.biluta.graph;
 
-public class Graph {
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Graph<T> {
+    private final T[] graphNodes;
+    private final int[][] connectivityMatrix;
+
+    public Graph(T[] graphNodes, int[][] connectivityMatrix) {
+        if (connectivityMatrix.length != connectivityMatrix[0].length) {
+            throw new IllegalArgumentException(String.format("Connectivity matrix has different width %d and height %d",
+                    connectivityMatrix[0].length, connectivityMatrix.length));
+        }
+
+        if (graphNodes.length != connectivityMatrix.length) {
+            throw new IllegalArgumentException(String.format("Nodes count %d not equals to connectivity matrix dimension %d.",
+                    graphNodes.length, connectivityMatrix.length));
+        }
+
+        this.graphNodes = graphNodes;
+        this.connectivityMatrix = connectivityMatrix;
+    }
+
+    public void breadthTraversal() {
+        Queue<T> queue = new LinkedList<>();
+        boolean[] isVisited = new boolean[graphNodes.length];
+
+        for (int i = 0; i < graphNodes.length; ++i) {
+            if (!isVisited[i]) {
+                queue.add(graphNodes[i]);
+                isVisited[i] = true;
+
+                while (!queue.isEmpty()) {
+                    T node = queue.poll();
+
+                    // Do some work with a node from the queue
+                    System.out.print(node);
+                    System.out.print(" ");
+
+                    for (int j = 0; j < isVisited.length; ++j) {
+                        if (connectivityMatrix[j][i] != 0 && !isVisited[j]) {
+                            queue.add(graphNodes[j]);
+                            isVisited[j] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- Graph/src/ru/academits/biluta/graph_main/Main.java
+++ Graph/src/ru/academits/biluta/graph_main/Main.java
@@ -1,4 +1,22 @@
 package ru.academits.biluta.graph_main;
 
+import ru.academits.biluta.graph.Graph;
+
 public class Main {
+    public static void main(String[] args) {
+        int[][] connections = {
+                {0, 1, 0, 0, 0, 0, 0},
+                {1, 0, 1, 1, 1, 1, 0},
+                {0, 1, 0, 0, 0, 0, 1},
+                {0, 1, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 1, 0},
+                {0, 1, 0, 0, 1, 0, 1},
+                {0, 0, 1, 0, 0, 1, 0}
+        };
+
+        Integer[] graphElements = {1, 2, 3, 4, 5, 6, 7};
+        Graph<Integer> graph = new Graph<>(graphElements, connections);
+
+        graph.breadthTraversal();
+    }
 }
