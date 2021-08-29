76f452d2b61df0a349ea0fb46dc7bcf6f18a7dda
Maksim Biluta <biluta.m.a@gmail.com>
2021-08-29 02:03:23 +0200

    Graph

--- Graph/src/ru/academits/biluta/graph_main/Main.java
+++ Graph/src/ru/academits/biluta/graph_main/Main.java
@@ -4,7 +4,7 @@ import ru.academits.biluta.graph.Graph;
 
 public class Main {
     public static void main(String[] args) {
-        String[] graphElements = {"A", "B", "E", "D", "C", "F", "G", "H", "J", "X", "Y", "Z", "K"};
+        String[] nodes = {"A", "B", "E", "D", "C", "F", "G", "H", "J", "X", "Y", "Z", "K"};
         int[][] connections = {
                 {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -21,7 +21,7 @@ public class Main {
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
         };
 
-        Graph<String> graph = new Graph<>(graphElements, connections);
+        Graph<String> graph = new Graph<>(nodes, connections);
 
         System.out.println("Graph breath traversal:");
         graph.breadthTraversal();
