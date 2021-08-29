10757252a6190952414fdb78049a9d415dae986d
Maksim Biluta <biluta.m.a@gmail.com>
2021-08-29 02:04:10 +0200

    Graph

--- Graph/src/ru/academits/biluta/graph/Graph.java
+++ Graph/src/ru/academits/biluta/graph/Graph.java
@@ -5,39 +5,39 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class Graph<T> {
-    private final T[] graphNodes;
+    private final T[] nodes;
     private final int[][] connectivityMatrix;
 
-    public Graph(T[] graphNodes, int[][] connectivityMatrix) {
+    public Graph(T[] nodes, int[][] connectivityMatrix) {
         if (connectivityMatrix.length != connectivityMatrix[0].length) {
             throw new IllegalArgumentException(String.format("Connectivity matrix has different width %d and height %d",
                     connectivityMatrix[0].length, connectivityMatrix.length));
         }
 
-        if (graphNodes.length != connectivityMatrix.length) {
+        if (nodes.length != connectivityMatrix.length) {
             throw new IllegalArgumentException(String.format("Nodes count %d is not equal to connectivity matrix dimension %d.",
-                    graphNodes.length, connectivityMatrix.length));
+                    nodes.length, connectivityMatrix.length));
         }
 
-        this.graphNodes = graphNodes;
+        this.nodes = nodes;
         this.connectivityMatrix = connectivityMatrix;
     }
 
     public void breadthTraversal() {
         Queue<T> nodesQueue = new LinkedList<>();
         Queue<Integer> indexQueue = new LinkedList<>();
-        boolean[] isVisited = new boolean[graphNodes.length];
+        boolean[] isVisited = new boolean[nodes.length];
 
         int branchCounter = 1;
 
-        for (int i = 0; i < graphNodes.length; ++i) {
+        for (int i = 0; i < nodes.length; ++i) {
             if (isVisited[i]) {
                 continue;
             }
 
             System.out.printf("Branch %d: ", branchCounter);
 
-            nodesQueue.add(graphNodes[i]);
+            nodesQueue.add(nodes[i]);
             indexQueue.add(i);
             isVisited[i] = true;
 
@@ -53,7 +53,7 @@ public class Graph<T> {
                 // put all unvisited children of the node to the queue by index
                 for (int j = 0; j < isVisited.length; ++j) {
                     if (connectivityMatrix[nodeIndex][j] != 0 && !isVisited[j]) {
-                        nodesQueue.add(graphNodes[j]);
+                        nodesQueue.add(nodes[j]);
                         indexQueue.add(j);
                         isVisited[j] = true;
                     }
@@ -68,18 +68,18 @@ public class Graph<T> {
     public void depthTraversal() {
         Stack<T> nodesStack = new Stack<>();
         Stack<Integer> indexStack = new Stack<>();
-        boolean[] isVisited = new boolean[graphNodes.length];
+        boolean[] isVisited = new boolean[nodes.length];
 
         int branchCounter = 1;
 
-        for (int i = 0; i < graphNodes.length; ++i) {
+        for (int i = 0; i < nodes.length; ++i) {
             if (isVisited[i]) {
                 continue;
             }
 
             System.out.printf("Branch %d: ", branchCounter);
 
-            nodesStack.add(graphNodes[i]);
+            nodesStack.add(nodes[i]);
             indexStack.add(i);
             isVisited[i] = true;
 
@@ -95,7 +95,7 @@ public class Graph<T> {
                 // put all unvisited children of the node to the stack in reverse order by index
                 for (int j = isVisited.length - 1; j >= 0; --j) {
                     if (connectivityMatrix[nodeIndex][j] != 0 && !isVisited[j]) {
-                        nodesStack.add(graphNodes[j]);
+                        nodesStack.add(nodes[j]);
                         indexStack.add(j);
                         isVisited[j] = true;
                     }
