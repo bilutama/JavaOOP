21549fbc81f8b086007f99a48f8f7f00f88d4732
bilutama <biluta.m.a@gmail.com>
2021-09-02 18:46:51 +0200

    Temperature

--- Temperature/src/ru/academits/biluta/temperature_main/Main.java
+++ Temperature/src/ru/academits/biluta/temperature_main/Main.java
@@ -1,13 +1,19 @@
 package ru.academits.biluta.temperature_main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("My first GUI");
+            JFrame frame = new JFrame("Frame");
             frame.setSize(300, 200);
+            frame.setLocationRelativeTo(null);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JButton okButton = new JButton("Convert");
+            frame.add(okButton, BorderLayout.SOUTH);
+
             frame.setVisible(true); // показать фрейм
         });
     }
