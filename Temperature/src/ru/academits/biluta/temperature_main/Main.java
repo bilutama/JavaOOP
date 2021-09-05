33223736c3fede7b04e5d69b8c7704f751401201
Maksim Biluta <biluta.m.a@gmail.com>
2021-09-06 01:21:17 +0200

    Temperature

--- Temperature/src/ru/academits/biluta/temperature_main/Main.java
+++ Temperature/src/ru/academits/biluta/temperature_main/Main.java
@@ -6,14 +6,19 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Frame");
+            JFrame frame = new JFrame("Temperature converter");
 
-            frame.setSize(300, 200);
+            frame.setSize(320, 200);
             frame.setLocationRelativeTo(null);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            JButton okButton = new JButton("Convert");
-            frame.add(okButton, BorderLayout.SOUTH);
+            JLabel label = new JLabel("Choose input and output scales and enter a value:");
+            label.setVerticalTextPosition(JLabel.TOP);
+            label.setHorizontalTextPosition(JLabel.CENTER);
+            frame.add(label, BorderLayout.NORTH);
+
+            JButton convertButton = new JButton("Convert");
+            frame.add(convertButton, BorderLayout.SOUTH);
 
             frame.setVisible(true); // показать фрейм
         });
