122e2d5d357d092b96c886bf331204feb9d88977
bilutama <biluta.m.a@gmail.com>
2021-09-06 17:59:38 +0200

    Temperature

--- Temperature/src/ru/academits/biluta/temperature_main/Main.java
+++ Temperature/src/ru/academits/biluta/temperature_main/Main.java
@@ -8,15 +8,21 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Temperature converter");
 
+            JPanel panel = new JPanel(new GridBagLayout());
+            GridBagConstraints constraints = new GridBagConstraints();
+
+            // set a FRAME
             frame.setSize(320, 200);
             frame.setLocationRelativeTo(null);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+            // set and enable a LABEL
             JLabel label = new JLabel("Choose input and output scales and enter a value:");
             label.setVerticalTextPosition(JLabel.TOP);
             label.setHorizontalTextPosition(JLabel.CENTER);
-            frame.add(label, BorderLayout.NORTH);
+            frame.add(label);
 
+            // set and enable a BUTTON
             JButton convertButton = new JButton("Convert");
             frame.add(convertButton, BorderLayout.SOUTH);
 
