d9530d20edcc76c6aca13ba46cbc88eb115c6889
bilutama <biluta.m.a@gmail.com>
2021-09-07 18:13:27 +0200

    Temperature

--- Temperature/src/ru/academits/biluta/model/TemperatureConverter.java
+++ Temperature/src/ru/academits/biluta/model/TemperatureConverter.java
@@ -1,5 +1,10 @@
 package ru.academits.biluta.model;
 
 public class TemperatureConverter {
+    private static final double ABSOLUTE_ZERO_CELSIUS = -273.15;
+    public enum Scales {KELVIN, CELSIUS, FAHRENHEIT};
 
+    public static void convertTemperature(double value, Scales convertFrom, Scales convertTo) {
+
+    }
 }
