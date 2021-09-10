9eaeff53cb3a7157f49cf3c9821d053a06b75eb2
bilutama <Borntobewild282>
2021-09-10 21:30:40 +0200

    Tree

--- Tree/src/ru/academits/biluta/people/PersonsComparator.java
+++ Tree/src/ru/academits/biluta/people/PersonsComparator.java
@@ -6,7 +6,7 @@ public class PersonsComparator implements Comparator<Person> {
     @Override
     public int compare(Person p1, Person p2) {
         if (p1 == null && p2 == null) {
-            return 1;
+            return 0;
         } else if (p1 == null) {
             return -1;
         } else if (p2 == null) {
